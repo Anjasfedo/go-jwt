633e6a552b4f86db1bff495806d9626e5cb20752
AnjasFedo <fedoaf98@gmail.com>
2024-03-12 21:58:06 +0700

    Create Connection to Database

---
+++ models/koneksi.go
@@ -0,0 +1,14 @@
+package models
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func Connect() {
+	dsn := "root:@tcp(127.0.0.1:3306)/go_jwt?charset=utf8mb4&parseTime=True&loc=Local"
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Couldn't connect to database")
+	}
+}
