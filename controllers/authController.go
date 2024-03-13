593f1277e29a3adc36af0d25c55ec97aa4d3e95c
AnjasFedo <fedoaf98@gmail.com>
2024-03-13 22:14:34 +0700

    Login Handler

--- controllers/authController.go
+++ controllers/authController.go
@@ -27,3 +27,31 @@ func Register(c *fiber.Ctx) error {
 
 	return c.JSON(user)
 }
+
+func Login(c *fiber.Ctx) error {
+	var data map[string]string
+
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+
+	var user models.User
+
+	database.DB.Where("email = ?", data["email"]).First(&user)
+
+	if user.Id == 0 {
+		c.Status(fiber.StatusNotFound)
+		return c.JSON(fiber.Map{
+			"message": "User not found",
+		})
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(data["password"])); err != nil {
+		c.Status(fiber.StatusBadRequest)
+		return c.JSON(fiber.Map{
+			"message": "Incorrect Password",
+		})
+	}
+
+	return c.JSON(user)
+}
